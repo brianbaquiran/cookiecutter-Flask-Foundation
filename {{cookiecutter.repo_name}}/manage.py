bf30bc5a2c32324dc1441a44441ff650496c1e18
Brian Baquiran <baquiran@pez.ai>
2017-03-27 00:53:26 +0800

    Fix usage of depercated flask.ext

--- {{cookiecutter.repo_name}}/manage.py
+++ {{cookiecutter.repo_name}}/manage.py
@@ -2,8 +2,8 @@
 
 import os
 
-from flask.ext.script import Manager, Server
-from flask.ext.script.commands import ShowUrls, Clean
+from flask_script import Manager, Server
+from flask_script.commands import ShowUrls, Clean
 from {{cookiecutter.repo_name}} import create_app
 from {{cookiecutter.repo_name}}.models import db, User
 
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/controllers/main.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/controllers/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, flash, request, redirect, url_for
-from flask.ext.login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required
 
 from {{cookiecutter.repo_name}}.extensions import cache
 from {{cookiecutter.repo_name}}.forms import LoginForm
